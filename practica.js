0faf6b152dbca1c2d85b5bd7583393607a5d40e4
unknown <lbelliski@gmail.com>
2019-10-22 00:14:38 -0300

    ejercicio clase 32

---
+++ practica.js
@@ -0,0 +1,6 @@
+let ingresos = 1000
+let ganancias = 600
+let impuestos = 500
+console.log ("El objetivo del mes:", ingresos > 800);
+console.log ("Se logró el objetivo impositivo", impuestos < 400);
+console.log ("Festejen que pagamos el bono!!", ganancias = true);
