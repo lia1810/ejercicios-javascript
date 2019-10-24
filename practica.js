6eea7a20dfc723d96cfb0f1d657fc4d188f056aa
unknown <lbelliski@gmail.com>
2019-10-23 22:01:22 -0300

    segundo commit

--- practica.js
+++ practica.js
@@ -1,6 +1,20 @@
+//ejercicio 34//
+
 let ingresos = 1000
 let ganancias = 600
 let impuestos = 500
 console.log ("El objetivo del mes:", ingresos > 800);
 console.log ("Se logró el objetivo impositivo", impuestos < 400);
 console.log ("Festejen que pagamos el bono!!", ganancias = true);
+//ejercicio35//
+
+let numeroDeCliente = 143245;
+let numeroDeClienteIngresado = 143245;
+
+
+
+//ejercicio clase 33//
+const nombre = prompt("Escribí tu nombre");
+const mes = prompt ("")
+const año = prompt("Escribí tu año de nacimiento");
+console.log("Tu edad es ", 2019 - edad);
