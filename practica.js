fd8afc7e29d7f6bb7cee06a0e6f82411eaae62e4
unknown <lbelliski@gmail.com>
2019-10-24 00:57:03 -0300

    sgregue hasta el 34

--- practica.js
+++ practica.js
@@ -10,10 +10,36 @@ console.log("El resto es igual a ", numero1 % numero2);
 let numero = 9;
 console.log("Tabla del 9 ", numero, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10);
 
+//ejercico 29//
+let lado = 10
+console.log("El perimetro del cuadrado es igual a ", lado * 4);
 
+//ejercicio 30//
+let ladoDelCuadrado = 5
+console.log("El area del cuadrado es igual a ", ladoDelCuadrado ** 2)
 
+//ejercicio 31//
+let lado1 = 10;
+let lado2 = 20;
+let lado3 = 5;
+console.log(`El perímetro del triángulo es ${lado1 + lado2 + lado3} `)
+//NO PUEDO CREER QUE ESCRIBI ESO Y SALIO!!!//
 
+//ejercicio 32//
+let altura = 10;
+let base = 4;
+console.log(`El perímetro  es ${ (base + altura) * 2 } `);
+console.log(`El área es igual a ${ base * altura}`);
 
+//ejercicio 33//
+
+let cantidadDePersonas = 100
+cantidadDePersonas += 5;
+console.log("La cantidad es " + cantidadDePersonas + " personas");
+cantidadDePersonas -= 3;
+console.log("La cantidad es " + cantidadDePersonas + " personas");
+cantidadDePersonas = cantidadDePersonas * 2;
+console.log("La cantidad es de " + cantidadDePersonas + " personas");
 
 //ejercicio 34//
 
@@ -23,12 +49,19 @@ let impuestos = 500
 console.log("El objetivo del mes:", ingresos > 800);
 console.log("Se logró el objetivo impositivo", impuestos < 400);
 console.log("Festejen que pagamos el bono!!", ganancias = true);
+
+
 //ejercicio35//
 
 let numeroDeCliente = 143245;
-let numeroDeClienteIngresado = 143245;
+let numeroDeClienteIngresado = `143245`;
+console.log (`El numero ingresado por el cliente es correcto ${numeroDeCliente == numeroDeClienteIngresado }`)
+console.log (`El numero ingresado por el cliente es correcto ${numeroDeCliente === numeroDeClienteIngresado }`)
 
-
+//ejercicio 36//
+let numerito1 = 10;
+let numerito2 = 20;
+console.log ( "Las variables tienen el mismo valor " )
 
 //ejercicio clase 33//
 // const nombre = prompt("Escribí tu nombre");
