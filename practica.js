e976eee985c8ce33bfa61b96981c2639e0cbb588
unknown <lbelliski@gmail.com>
2019-10-24 00:13:33 -0300

    agregando ejercicios

--- practica.js
+++ practica.js
@@ -1,11 +1,28 @@
+//ejercicio 27//
+let numero1 = 5;
+let numero2 = 7;
+console.log("El resultado de la suma es ", numero1 + numero2)
+console.log("El resultado de la resta es ", numero1 - numero2);
+console.log("El producto de la multiplicacion es ", numero1 * numero2);
+console.log("La division da como resultado ", numero1 / numero2);
+console.log("El resto es igual a ", numero1 % numero2);
+//ejercicico 28//
+let numero = 9;
+console.log("Tabla del 9 ", numero, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10);
+
+
+
+
+
+
 //ejercicio 34//
 
 let ingresos = 1000
 let ganancias = 600
 let impuestos = 500
-console.log ("El objetivo del mes:", ingresos > 800);
-console.log ("Se logró el objetivo impositivo", impuestos < 400);
-console.log ("Festejen que pagamos el bono!!", ganancias = true);
+console.log("El objetivo del mes:", ingresos > 800);
+console.log("Se logró el objetivo impositivo", impuestos < 400);
+console.log("Festejen que pagamos el bono!!", ganancias = true);
 //ejercicio35//
 
 let numeroDeCliente = 143245;
@@ -14,7 +31,7 @@ let numeroDeClienteIngresado = 143245;
 
 
 //ejercicio clase 33//
-const nombre = prompt("Escribí tu nombre");
-const mes = prompt ("")
-const año = prompt("Escribí tu año de nacimiento");
-console.log("Tu edad es ", 2019 - edad);
+// const nombre = prompt("Escribí tu nombre");
+// const mes = prompt("Escribi tu mes de nacimiento");
+// const año = prompt("Escribí tu año de nacimiento");
+// console.log("Tu edad es ", 2019 - año);
